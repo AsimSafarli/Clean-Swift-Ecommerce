7c3cdfbfc8cf0061a7eb6941e1d20a1a245b0be1
Asim <saffarrli14@gmail.com>
2024-03-07 10:48:54 +0400

    add

---
+++ E-commerce-Swift/Uitls/Component/CustomMainTabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CustomMainTabBarViewController.swift
+//  E-commerce-Swift
+//
+//  Created by Asim Seferli on 07.03.24.
+//
+
+import UIKit
+
+class CustomMainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ E-commerce-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E-commerce-Swift
+//
+//  Created by Asim Seferli on 07.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
